6b8a55e545cbeaa5a2140d5ad16ee503a06968ff
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-26 17:45:12 -0600

    assignment complete.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+fn eat_meal(mut meal: String) -> String {
+    println!("{meal}");
+    meal.clear();
+    println!("{meal}");
+    meal
+}
+
 fn main() {
-    println!("Hello, world!");
+    let is_concert: bool = true;
+    let is_event: bool = is_concert;
+    // the ownership is independent through the copy trait 
+    println!("{is_concert}{is_event}");
+
+    let sushi: &str = "Salmon";
+    let dinner: &str = sushi;
+// I dont think it will transfer ownership because strings do not possess the copy trait.
+
+println!("{sushi} {dinner}");
+
+    let orange: String = String::from("grapes");
+    let apple: String = orange;
+
+    println!("{apple}");
+
+    println!("{}", eat_meal(apple))
+
+
 }
